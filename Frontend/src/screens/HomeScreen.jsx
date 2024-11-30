25028ef69a0af7ba1e1bd6564d967a6ad9c04928
satish-pradhn <pradhan.satish2025@gmail.com>
2024-11-30 16:10:02 +0545

    Add HomeScreen component to display homepage content

---
+++ Frontend/src/screens/HomeScreen.jsx
@@ -0,0 +1,18 @@
+import Product from "../components/Product";
+import { Row, Col } from "react-bootstrap";
+import products from "../../products";
+const HomeScreen = function () {
+  return (
+    <>
+      <h1> Latest Products</h1>
+      <Row >
+        {products.map((product) => (
+          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+            <Product product={product} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
+};
+export default HomeScreen;
