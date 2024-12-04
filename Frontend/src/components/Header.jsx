29975db2780a9d4ccbf200e70cda3140ffc924da
satish-pradhn <pradhan.satish2025@gmail.com>
2024-12-04 12:33:19 +0545

    Update link component for better navigation

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -1,31 +1,27 @@
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Header = function () {
   return (
-    <>
-      <header>
-        <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
-          <Container>
-            {" "}
-            {/* Ensure the elements inside the navbar doesn't stretch to the end*/}
-            <Navbar.Brand href="/">My Shop</Navbar.Brand>{" "}
-            {/* sub component of navbar*/}
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="ms-auto">
-                <Nav.Link href="/cart">
-                  <FaShoppingCart /> Cart
-                </Nav.Link>
-                <Nav.Link href="/login">
-                  <FaUser /> Sign In
-                </Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
-      </header>
-    </>
+    <header>
+      <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
+        <Container>
+          <Navbar.Brand as={Link} to="/">My Shop</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link  as={Link} to="/cart">
+                <FaShoppingCart /> Cart
+              </Nav.Link>
+              <Nav.Link as={Link} to="/login">
+                <FaUser /> Sign In
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
   );
 };
 export default Header;
